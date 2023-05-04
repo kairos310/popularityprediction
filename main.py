1409b67b1cbe67e1020e0fa2a7a25701181a1d89
wilds-23 <donwilson1029@gmail.com>
2023-05-04 16:19:57 -0500

    Find most important features using knn

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-
 files = ["webscraping/Billboard Hot 100.csv", "webscraping/BUTTER.csv", "webscraping/Electric Lady Studios.csv", "webscraping/Fresh Finds.csv", "webscraping/Best of the Decade For You.csv"]
 frames = []
 for f in files:
@@ -47,6 +46,8 @@ def nearestNeighbors(k, X_data, y_data, toPredict):
     for i in range(len(X_data)):
         currMax = max(closest.keys())
         distance = dist(X_data[i], X_data[toPredict])
+        if distance==0:
+            continue
         if distance < currMax:
             # HERE WE CAN JUST ADD SONG, TITLE AFTER Y_DATA TO HAVE DICTIONARY STORE A TRIPLE OF (POPULARITY, SONG, TITLE)
             closest[distance] = y_data[i]
@@ -55,6 +56,54 @@ def nearestNeighbors(k, X_data, y_data, toPredict):
     total = 0
     for key in closest:
         total += closest.get(key)
-    return(total/len(closest.keys()))
+    return (total/len(closest.keys())), y_data[toPredict]
 
-print(nearestNeighbors(5, X_train_normed_initial, y_train, 0))
+def computeError(prediction, actual):
+    return (prediction - actual)**2
+
+def computeAccuracy(k, X_data, y_data):
+    error = 0
+    for i in range(len(X_train_normed_initial)):
+        prediction, actual = nearestNeighbors(k, X_data, y_data, i)
+        error += computeError(prediction, actual)
+    return error
+
+bestSingle = {}
+bestDuo = {}
+bestTrio = {}
+for i in range(len(X_train_normed_initial[0])-2):
+    
+    print("Next i value: ",i)
+    X_train_extracted = X_train_normed_initial[:, [i]]
+    bestSingle[i] = computeAccuracy(10, X_train_extracted, y_train)
+
+    for j in range(i+1, len(X_train_normed_initial[0])-1):
+        
+        X_train_extracted = X_train_normed_initial[:, [i, j]]
+        bestDuo[i,j] = computeAccuracy(10, X_train_extracted, y_train)
+        
+        for k in range(j+1, len(X_train_normed_initial[0])):
+
+            X_train_extracted = X_train_normed_initial[:, [i, j, k]]
+            bestTrio[i,j,k] = computeAccuracy(10, X_train_extracted, y_train)
+
+for i in range(len(X_train_normed_initial[0])-2,len(X_train_normed_initial[0])-1):
+    
+    print("Next i value: ",i)
+    X_train_extracted = X_train_normed_initial[:, [i]]
+    bestSingle[i] = computeAccuracy(10, X_train_extracted, y_train)
+    
+    for j in range(i, len(X_train_normed_initial[0])):
+
+        X_train_extracted = X_train_normed_initial[:, [i, j]]
+        bestDuo[i,j] = computeAccuracy(10, X_train_extracted, y_train)
+
+X_train_extracted = X_train_normed_initial[:, [len(X_train_normed_initial[0])-1]]
+bestSingle[len(X_train_normed_initial[0])-1] = computeAccuracy(10, X_train_extracted, y_train)
+
+print("----------Singles---------")
+print(sorted( ((v,k) for k,v in bestSingle.items())))
+print("----------Doubles---------")
+print(sorted( ((v,k) for k,v in bestDuo.items())))
+print("----------Triples---------")
+print(sorted( ((v,k) for k,v in bestTrio.items())))
